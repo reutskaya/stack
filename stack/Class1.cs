b903cd88894c45f77c1d598987db6e02475fdf3e
reutskaya <32382007+reutskaya@users.noreply.github.com>
2017-10-06 22:10:31 +0300

    Initial commit

---
+++ stack/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Stack<T>
+    {
+        private T[] array;
+        private const int defaultCapacity = 30;
+        private int size;
+
+        public Stack()
+        {
+            this.size = 0;
+            this.array = new T[defaultCapacity];
+        }
+
+        public bool Empty() => this.size == 0;
+
+        public virtual int Count => this.size;
+
+        public T Pop()
+        {
+            if (this.size == 0)
+            {
+                Console.WriteLine("Stack is empty");
+                throw new Exception();
+            }
+
+            var value = this.array[this.size - 1];
+            this.array[this.size - 1] = default(T);
+            this.size--;
+            return value;
+        }
+
+        public void Push(T element) =>
+                this.array[this.size++] = element;
+        
+        public T TakeElement()
+        {
+            if (this.size == 0)
+            {
+                Console.WriteLine("Stack is empty");
+                throw new Exception();
+            }
+
+            return this.array[this.size - 1];
+        }
+    }
+}
---
+++ stack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var stack = new Stack<int>();
+
+            Console.WriteLine(stack.Empty());
+
+            stack.Push(7);
+            stack.Push(13);
+            stack.Push(2);
+            var head = stack.Pop();
+            Console.WriteLine(head);
+
+            head = stack.TakeElement();
+            Console.WriteLine(head);
+
+            Console.WriteLine(stack.Empty());
+
+            Console.ReadKey();
+        }
+    }
+}
